65c5bb2a8c99fe15ae3f09e69bb141c864605d73
Joelz9614 <joelz9614@gmail.com>
2012-12-04 23:13:36 -0700

    updates

--- d2vzw-vendor-blobs.mk
+++ d2vzw-vendor-blobs.mk
@@ -58,6 +58,10 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2vzw/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
         vendor/samsung/d2vzw/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
         vendor/samsung/d2vzw/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+        vendor/samsung/d2spr/proprietary/lib/libsc-a3xx.so:system/lib/libsc-a3xx.so \
+        vendor/samsung/d2spr/proprietary/lib/libllvm-a3xx.so:system/lib/libllvm-a3xx.so \
+        vendor/samsung/d2spr/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
+        vendor/samsung/d2spr/proprietary/lib/libc2d2_a3xx.so:system/lib/libc2d2_a3xx.so \
         vendor/samsung/d2vzw/proprietary/bin/netmgrd:system/bin/netmgrd \
         vendor/samsung/d2vzw/proprietary/bin/sec-ril:system/bin/sec-ril \
         vendor/samsung/d2vzw/proprietary/bin/qmiproxy:system/bin/qmiproxy \
@@ -162,6 +166,14 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2vzw/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
         vendor/samsung/d2vzw/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
         vendor/samsung/d2vzw/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+        vendor/samsung/d2spr/proprietary/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
+        vendor/samsung/d2spr/proprietary/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+        vendor/samsung/d2spr/proprietary/etc/firmware/vidc.b00:system/etc/firmware/vidc.b00 \
+        vendor/samsung/d2spr/proprietary/etc/firmware/vidc.b01:system/etc/firmware/vidc.b01 \
+        vendor/samsung/d2spr/proprietary/etc/firmware/vidc.b02:system/etc/firmware/vidc.b02 \
+        vendor/samsung/d2spr/proprietary/etc/firmware/vidc.b03:system/etc/firmware/vidc.b03 \
+        vendor/samsung/d2spr/proprietary/etc/firmware/vidc.mdt:system/etc/firmware/vidc.mdt \
+        vendor/samsung/d2spr/proprietary/etc/firmware/vidcfw.elf:system/etc/firmware/vidcfw.elf \
         vendor/samsung/d2vzw/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
         vendor/samsung/d2vzw/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so
 
