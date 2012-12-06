48df8f40604dfa2131628c0fb53484f0cd5a22b1
Joelz9614 <joelz9614@gmail.com>
2012-12-05 21:57:26 -0700

    add some missing blobs

--- d2vzw-vendor-blobs.mk
+++ d2vzw-vendor-blobs.mk
@@ -44,6 +44,8 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2vzw/proprietary/lib/libdivxdrm.so:system/lib/libdivxdrm.so \
         vendor/samsung/d2vzw/proprietary/media/bootanimation.zip:system/media/bootanimation.zip \
         vendor/samsung/d2vzw/proprietary/media/bamf_audio.mp3:system/media/bamf_audio.mp3 \
+        vendor/samsung/d2vzw/proprietary/media/LMprec_508.emd:system/media/LMprec_508.emd \
+        vendor/samsung/d2vzw/proprietary/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
         vendor/samsung/d2vzw/proprietary/xbin/busybox:system/xbin/busybox \
         vendor/samsung/d2vzw/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
         vendor/samsung/d2vzw/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
