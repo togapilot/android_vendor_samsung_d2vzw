42850886b8fd2cf6a0a9c5dbbaf06e6951c52b0f
boss <boss@boss-Inspiron-1545.(none)>
2012-12-23 01:47:37 -0800

    remove encoding libs

--- d2vzw-vendor-blobs.mk
+++ d2vzw-vendor-blobs.mk
@@ -57,10 +57,6 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2vzw/proprietary/lib/libqmi.so:system/lib/libqmi.so \
         vendor/samsung/d2vzw/proprietary/lib/libqdi.so:system/lib/libqdi.so \
         vendor/samsung/d2vzw/proprietary/lib/libqdp.so:system/lib/libqdp.so \
-        vendor/samsung/d2vzw/proprietary/lib/libOmxAacEnc.so:system/lib/libOmxAacEnc.so \
-        vendor/samsung/d2vzw/proprietary/lib/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
-        vendor/samsung/d2vzw/proprietary/lib/libOmxEvrcEnc.so:system/lib/libOmxEvrcEnc.so \
-        vendor/samsung/d2vzw/proprietary/lib/libOmxQcelp13Enc.so:system/lib/libOmxQcelp13Enc.so \
         vendor/samsung/d2vzw/proprietary/lib/libqmi_client_qmux.so:system/lib/libqmi_client_qmux.so \
         vendor/samsung/d2vzw/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
         vendor/samsung/d2vzw/proprietary/lib/libril.so:system/lib/libril.so \
